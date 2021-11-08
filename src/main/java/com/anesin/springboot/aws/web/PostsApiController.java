42ad5624b5ae1e7803a56629ea13614df7d5fc99
이재영 <jaeyoung.yi@seculetter.com>
2021-11-09 00:04:30 +0900

    Chapter 03 스프링 부트에서 JPA로 데이터베이스 다뤄보자 3.4 등록/수정/조회 API 만들기
    - 도메인 모델 기반으로 수정/조회 API 구현
    - 웹 콘솔을 사용 (application.properties 수정)

--- src/main/java/com/anesin/springboot/aws/web/PostsApiController.java
+++ src/main/java/com/anesin/springboot/aws/web/PostsApiController.java
@@ -1,11 +1,11 @@
 package com.anesin.springboot.aws.web;
 
 import com.anesin.springboot.aws.service.posts.PostsService;
+import com.anesin.springboot.aws.web.dto.PostsResponseDto;
 import com.anesin.springboot.aws.web.dto.PostsSaveRequestDto;
+import com.anesin.springboot.aws.web.dto.PostsUpdateRequestDto;
 import lombok.RequiredArgsConstructor;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RequiredArgsConstructor
@@ -15,9 +15,21 @@ public class PostsApiController {
   private final PostsService postsService;
 
 
-  @PostMapping("api/v1/posts")
+  @PostMapping("/api/v1/posts")
   public Long save(@RequestBody PostsSaveRequestDto requestDto) {
     return postsService.save(requestDto);
+  }
+
+
+  @PutMapping("/api/v1/posts/{id}")
+  public Long update(@PathVariable Long id, @RequestBody PostsUpdateRequestDto requestDto) {
+    return postsService.update(id, requestDto);
+  }
+
+
+  @GetMapping("/api/v1/posts/{id}")
+  public PostsResponseDto findById(@PathVariable Long id) {
+    return postsService.findById(id);
   }
 
 }
---
+++ src/main/java/com/anesin/springboot/aws/web/dto/PostsResponseDto.java
@@ -0,0 +1,23 @@
+package com.anesin.springboot.aws.web.dto;
+
+import com.anesin.springboot.aws.domain.posts.Posts;
+import lombok.Getter;
+
+
+@Getter
+public class PostsResponseDto {
+
+  private Long id;
+  private String title;
+  private String content;
+  private String author;
+
+
+  public PostsResponseDto(Posts entity) {
+    this.id      = entity.getId();
+    this.title   = entity.getTitle();
+    this.content = entity.getContent();
+    this.author  = entity.getAuthor();
+  }
+
+}
