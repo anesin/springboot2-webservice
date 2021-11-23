ce932bd0d40b7059a7b73f0f368cc6fe4ba17901
이재영 <jaeyoung.yi@seculetter.com>
2021-11-24 07:16:34 +0900

    Chapter 05 스프링 시큐리티와 OAuth 2.0으로 로그인 기능 구현하기 5.4 어노테이션 기반으로 개선하기
    - 어노테이션 생성
    - 세션값을 가져오는 부분이 반복될 것을 어노테이션으로 제거

--- src/main/java/com/anesin/springboot/aws/web/IndexController.java
+++ src/main/java/com/anesin/springboot/aws/web/IndexController.java
@@ -1,5 +1,6 @@
 package com.anesin.springboot.aws.web;
 
+import com.anesin.springboot.aws.config.auth.LoginUser;
 import com.anesin.springboot.aws.config.auth.dto.SessionUser;
 import com.anesin.springboot.aws.service.posts.PostsService;
 import com.anesin.springboot.aws.web.dto.PostsResponseDto;
@@ -9,21 +10,17 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
-import javax.servlet.http.HttpSession;
-
 
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
   private final PostsService postsService;
-  private final HttpSession httpSession;
 
 
   @GetMapping("/")
-  public String index(Model model) {
+  public String index(Model model, @LoginUser SessionUser user) {
     model.addAttribute("posts", postsService.findAllDesc());
-    SessionUser user = (SessionUser) httpSession.getAttribute("user");
     if (user != null)
       model.addAttribute("userName", user.getName());
     return "index";
